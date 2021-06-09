9a82be8b29dd631ccd53451707084de061b9ea33
Gus Gutoski <gus.gutoski@protonmail.com>
2021-06-09 14:51:57 -0400

    do not exclude keygen test

--- src/core.rs
+++ src/core.rs
@@ -583,10 +583,10 @@ mod tests {
         assert_eq!(m, BigInt::from(200).into());
     }
 
-    #[cfg(feature = "keygen")]
     #[test]
     fn test_correct_keygen() {
-        let (ek, dk): (EncryptionKey, _) = Paillier::keypair_with_modulus_size(2048).keys();
+        let (ek, dk): (EncryptionKey, _) =
+            Paillier::keypair_with_modulus_size(&mut rand::thread_rng(), 2048).keys();
 
         let m = RawPlaintext::from(BigInt::from(10));
         let c = Paillier::encrypt(&ek, m.clone()); // TODO avoid clone
